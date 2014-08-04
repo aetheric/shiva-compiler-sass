e835867fb5db4b79534be4d7b12e6bd0972c7e5a
pcummuskey <pcummuskey@bravurasolutions.com>
2014-08-04 17:40:52 +1200

    Added some more useful code to run the scripts.

--- src/main/java/nz/co/aetheric/shiva/compiler/sass/Functions.java
+++ src/main/java/nz/co/aetheric/shiva/compiler/sass/Functions.java
@@ -1,8 +1,11 @@
 package nz.co.aetheric.shiva.compiler.sass;
 
 import lombok.extern.slf4j.Slf4j;
+import org.jruby.embed.ScriptingContainer;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.net.URL;
 
@@ -25,6 +28,18 @@ public final class Functions {
 
 	public static File createTempFile(String suffix) throws IOException {
 		return File.createTempFile(String.valueOf(System.currentTimeMillis()), suffix);
+	}
+
+	public static Object runScript(ScriptingContainer scripting, File script) {
+		try (FileReader reader = new FileReader(script)) {
+			return scripting.runScriptlet(reader, script.getName());
+		} catch (FileNotFoundException error) {
+			log.error("Couldn't find the provided script.", error);
+			return null;
+		} catch (IOException error) {
+			log.error("Something went wrong loading the provided script.", error);
+			return null;
+		}
 	}
 
 	/** This class is non-instantiable. */
--- src/main/java/nz/co/aetheric/shiva/compiler/sass/SassCompilerService.java
+++ src/main/java/nz/co/aetheric/shiva/compiler/sass/SassCompilerService.java
@@ -1,8 +1,10 @@
 package nz.co.aetheric.shiva.compiler.sass;
 
+import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.FileUtils;
+import org.jruby.embed.ScriptingContainer;
 
 import java.io.*;
 
@@ -11,17 +13,30 @@ import java.io.*;
  * <p>Author: <a href="http://gplus.to/tzrlk">Peter Cummuskey</a></p>
  */
 @Slf4j
-@Setter
 public class SassCompilerService implements SassCompiler {
 
-	protected File scriptConfig;
-	protected File scriptInit;
-	protected File scriptCompile;
+	private final ScriptingContainer scripting;
+
+	@Getter
+	private File scriptConfig;
+
+	@Getter
+	private boolean initialised;
+
+	@Getter
+	@Setter
+	private File scriptInit;
+
+	@Getter
+	@Setter
+	private File scriptCompile;
 
 	public SassCompilerService() {
-		this.scriptConfig = Functions.getFile("sass/config.rb");
-		this.scriptInit = Functions.getFile("sass/init.rb");
-		this.scriptCompile = Functions.getFile("sass/compile.rb");
+		this.scripting = new ScriptingContainer();
+		this.setScriptConfig(Functions.getFile("sass/config.rb"));
+		this.setScriptInit(Functions.getFile("sass/init.rb"));
+		this.setScriptCompile(Functions.getFile("sass/compile.rb"));
+		initialised = false;
 	}
 
 	/** {@inheritDoc} */
@@ -43,7 +58,29 @@ public class SassCompilerService implements SassCompiler {
 	/** {@inheritDoc} */
 	@Override
 	public void compile(File input, File output) {
-		//TODO: Implement this.
+
+		if (!initialised) {
+			final Object initResult = Functions.runScript(scripting, scriptInit);
+			if (initResult == null) {
+				log.warn("Something went wrong during compiler initialisation.");
+				return;
+			}
+
+			initialised = true;
+		}
+
+		final Object compileResult = Functions.runScript(scripting, scriptCompile);
+		if (compileResult == null) {
+			log.warn("Something went wrong during compiler operation.");
+			return;
+		}
+
+		log.debug("Compilation of {} to {} succeeded.", input, output);
+	}
+
+	public void setScriptConfig(File scriptConfig) {
+		this.scriptConfig = scriptConfig;
+		scripting.put("configLocation", scriptConfig.getPath());
 	}
 
 }
